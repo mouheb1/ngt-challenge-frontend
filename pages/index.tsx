1f6ef462955f9d9d0577f2ff4982bdf6f7411215
mouheb.bzri <mouheb.bzri@gmail.com>
2021-07-22 02:57:42 +0100

    ✨ Implement the search feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
 import { Box, TextInput } from "grommet";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { fetchStudents, Student } from "../services/students";
 import { StudentCard } from "../components/studentCard";
 import { NextPage, NextPageContext } from "next";
 type Props = {
-  studentList?: Student[];
+  studentList?: Student[]
 };
 
 const Main: NextPage<Props> = ({ studentList = [] }) => {
-  const onChangeHandler = () => {};
+  const [filtredStudent, setFiltredStudent] = useState(studentList);
+  const onChangeHandler = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.currentTarget;
+      const newList = studentList.filter(
+        ({ first_name, last_name }) =>
+          first_name.toUpperCase().search(value.toUpperCase()) !== -1 ||
+          last_name.toUpperCase().search(value.toUpperCase()) !== -1
+      );
+      setFiltredStudent(newList);
+    },
+    [studentList]
+  );
 
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
       <TextInput placeholder="type here" onChange={onChangeHandler} />
       <Box direction="row" wrap={true} className="d-flex flex-center">
-        {studentList.map((student: Student) => (
+        {filtredStudent.map((student: Student) => (
           <StudentCard key={student.id} student={student} />
         ))}
       </Box>
